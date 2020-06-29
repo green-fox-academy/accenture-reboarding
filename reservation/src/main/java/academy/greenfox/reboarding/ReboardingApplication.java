e664fda0081079c15d013829842ed14a20782420
Kond <peter.tamas@greenfoxacademy.com>
2020-06-29 11:25:43 +0300

    feat(reservation): enable Eureka client

--- reservation/src/main/java/academy/greenfox/reboarding/ReboardingApplication.java
+++ reservation/src/main/java/academy/greenfox/reboarding/ReboardingApplication.java
@@ -2,9 +2,11 @@ package academy.greenfox.reboarding;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableSwagger2
 public class ReboardingApplication {
 
